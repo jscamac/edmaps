d8d2efcc5511b1d8fa495f876ecac8098f55179f
John Baumgartner <johnbaums@gmail.com>
2020-11-20 16:14:51 +1100

    Additional fix for captured_by_ncells

--- R/captured_by_ncells.R
+++ R/captured_by_ncells.R
@@ -17,9 +17,17 @@ captured_by_ncells <- function(infiles, names, n_cells, all=TRUE) {
   lst <- lapply(seq_along(infiles), function(x) {
     rast <- raster::stack(infiles[x])
     n_notna <- raster::cellStats(!is.na(rast), sum)
+    n <- min(n_cells, n_notna)
     q <- mapply(raster::quantile, raster::unstack(rast), 
                 pmax(0, 1 - n_cells/n_notna))
-    i <- lapply(raster::unstack(rast > q), raster::Which, cells=TRUE)
+    i <- mapply(function(test, r, q_) {
+      if(test) {
+        raster::Which(r >= q_, cells=TRUE)
+      } else {
+        raster::Which(r > q_, cells=TRUE)
+      }
+    }, n_cells >= n_notna, raster::unstack(rast), q, SIMPLIFY=FALSE)
+    
     vals <- mapply(function(r, i_) r[i_], raster::unstack(rast), i, 
                    SIMPLIFY=FALSE)
     total <- raster::cellStats(rast, sum)
@@ -27,12 +35,12 @@ captured_by_ncells <- function(infiles, names, n_cells, all=TRUE) {
     if (isTRUE(all)) {
       risk_captured <- lapply(vals, function(x) cumsum(sort(x, decreasing=TRUE)))
       proportion <- mapply(function(risk, tot) risk/tot, risk_captured, total)
-      data.frame(map = rep(names[x], n_cells), ncell = seq_len(n_cells), 
+      data.frame(map = rep(names[x], n), ncell = seq_len(n), 
                  proportion = proportion)
     } else {
       proportion <- mapply(function(v, tot) sum(v)/tot, vals, total, 
                            SIMPLIFY=FALSE)
-      data.frame(map = names[x], ncell = n_cells, 
+      data.frame(map = names[x], ncell = n, 
                  proportion = do.call(cbind, proportion))
     }
   })
