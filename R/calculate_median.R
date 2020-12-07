30a9632ab5742ab3a9eb89dc1c609b31d1bd5fd2
John Baumgartner <johnbaums@gmail.com>
2020-12-07 16:55:14 +1100

    Add fun to calculate cellwise median of multiple Raster* objects, layer by layer

---
+++ R/calculate_median.R
@@ -0,0 +1,26 @@
+#' Calculate median of raster objects
+#'
+#' Calculate the cellwise median of two or more Raster* objects, by layer.
+#'
+#' @param files A vector of file paths to (optionally multiband) raster files.
+#' @param outfile Character. Output raster file path. If not provided, object 
+#'   will be returned to R. Directory will be created recursively if it does 
+#'   not exist.
+#' @return If \code{outfile} is specified, the resulting raster is saved as a
+#'   geotiff to that path and NULL is returned. If \code{outfile} is not
+#'   specified the resulting raster is returned.
+#' @importFrom raster stack overlay writeRaster
+#' @export
+calculate_median <- function(files, outfile) {
+  result <- lapply(files, function(ff) {
+    ss <- lapply(ff, function(s) raster::stack(s))
+    do.call(function(...) raster::overlay(..., fun=median), ss)
+  })  
+  if(!dir.exists(dirname(outfile))) dir.create(dirname(outfile), recursive=TRUE)
+  if(!missing(outfile)) {
+    raster::writeRaster(result, outfile)
+    return(NULL)
+  } else {
+    result
+  }
+}
