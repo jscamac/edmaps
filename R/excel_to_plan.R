722d949efb7754f020c37cc65d9d8a7d82a57e2e
John Baumgartner <johnbaums@gmail.com>
2021-03-02 13:46:26 +1100

    Add missing namespace

--- R/excel_to_plan.R
+++ R/excel_to_plan.R
@@ -1,17 +1,17 @@
 #' Create a drake plan using tabular species data
 #'
-#' Import parameters from an Excel xlsx file and create a drake plan to 
+#' Import parameters from an Excel xlsx file and create a drake plan to
 #' estimate establishment likelihoods.
-#' 
-#' @param file Character. File path to an xlsx file containing required 
-#'   parameters for creating a species plan. See Details and 
+#'
+#' @param file Character. File path to an xlsx file containing required
+#'   parameters for creating a species plan. See Details and
 #'   \code{\link{species_plan}} for further information.
 #' @return A drake plan with a combined workflow for all species.
-#' @details To simplify reproducibility, \code{edmaps} provides an 
+#' @details To simplify reproducibility, \code{edmaps} provides an
 #'   \emph{Excel} interface for specifying species parameters relevant to
-#'   estimating establishment likelihood. An example spreadsheet is 
-#'   bundled with the package, available at the path given by 
-#'   \code{system.file('extdata/parameters.xlsx', package='edmaps')}. The 
+#'   estimating establishment likelihood. An example spreadsheet is
+#'   bundled with the package, available at the path given by
+#'   \code{system.file('extdata/parameters.xlsx', package='edmaps')}. The
 #'   spreadsheet has two sheets, the first specifying "global" parameters
 #'   that will apply to all species (e.g. file paths to rasters that will),
 #'   be used regardless of species identity and the second specifying
@@ -26,8 +26,8 @@
 #' @export
 excel_to_plan <- function(file) {
   globals <- readxl::read_excel(file, sheet='Global parameters', skip=1,
-                                col_types=c('text', 'skip', 'list')) %>% 
-    tidyr::spread(Variable, Value) %>% 
+                                col_types=c('text', 'skip', 'list')) %>%
+    tidyr::spread(Variable, Value) %>%
     # standardise variable names
     dplyr::select(make_interactive_maps=`Make interactive maps?`,
                   gbif_username=`GBIF username`,
@@ -54,15 +54,15 @@ excel_to_plan <- function(file) {
                   total_food=`Total food`,
                   total_goods=`Total goods`) %>% 
     lapply(unlist)
-  
+
   # Check that supplied file paths exist
   paths_global <- na.omit(unlist(globals[grep('_path$', names(globals))]))
   if(length(paths_global) > 0 && !all(file.exists(paths_global))) {
-    stop('File not found:\n    - ', 
+    stop('File not found:\n    - ',
          paste0(paths_global[!file.exists(paths_global)], collapse='\n    - '))
   }
-  
-  species <- readxl::read_excel(file, sheet='Species-specific parameters') %>% 
+
+  species <- readxl::read_excel(file, sheet='Species-specific parameters') %>%
     # standardise names
     dplyr::rename(species=Species,
                   species_group=`Species group`,
@@ -90,7 +90,7 @@ excel_to_plan <- function(file) {
                   prob_nurserystock=`Prob nurserystock`,
                   prob_food=`Prob food`,
                   prob_goods=`Prob goods`,
-                  aggregated_res=`Aggregated res`) %>% 
+                  aggregated_res=`Aggregated res`) %>%
     dplyr::mutate(
       species=gsub('^\\s+|\\s+$', '', species),
       species=gsub('\\s+', ' ', species),
@@ -103,48 +103,48 @@ excel_to_plan <- function(file) {
         gsub('^\\s+|\\s+$', '', exclude_bioclim_vars), '\\s*,\\s*'),
       infected_countries=gsub('^\\s+|\\s+$', '', infected_countries)
     )
-  
-  paths_species <- species %>% 
-    select(grep('_path$', colnames(.))) %>% 
-    unlist() %>% 
+
+  paths_species <- species %>%
+    dplyr::select(grep('_path$', colnames(.))) %>%
+    unlist() %>%
     setdiff(NA)
-  
+
   if(length(paths_species) > 0 && !all(file.exists(paths_species))) {
-    stop('File not found:\n    - ', 
+    stop('File not found:\n    - ',
          paste0(paths_species[!file.exists(paths_species)], collapse='\n    - '))
   }
-  
-  invalid_abiotic <- species$include_abiotic_weight & 
-    (is.na(species$occurrence_path) & 
-       is.na(species$gbif_species) & 
+
+  invalid_abiotic <- species$include_abiotic_weight &
+    (is.na(species$occurrence_path) &
+       is.na(species$gbif_species) &
        is.na(species$climate_suitability_path))
-  
+
   if(any(invalid_abiotic)) {
-    # ^ if abiotic weight is used, then if no occurrence/gbif data are used and 
+    # ^ if abiotic weight is used, then if no occurrence/gbif data are used and
     #   climate suitability path not provided, stop.
     stop('If include_abiotic_weight is TRUE, either climate_suitability_path ',
          'or else gbif_species and/or occurrence_path must be provided.')
   }
-  
-  cabi_paths_idx <- which(grepl('\\.csv$', species$infected_countries) & 
+
+  cabi_paths_idx <- which(grepl('\\.csv$', species$infected_countries) &
          species$include_abiotic_weight)
   exist <- file.exists(species$infected_countries[cabi_paths_idx])
-  
+
   if(any(!exist)) {
-    stop('File not found:\n    - ', 
-         paste0(species$infected_countries[cabi_paths_idx[!exist]], collapse='\n    - ')) 
+    stop('File not found:\n    - ',
+         paste0(species$infected_countries[cabi_paths_idx[!exist]], collapse='\n    - '))
   }
   species$cabi_path <- NA
   species$cabi_path[cabi_paths_idx] <- species$infected_countries[cabi_paths_idx]
   species$infected_countries[cabi_paths_idx] <- NA
-  
+
   # Test if any countries are invalid
   if(any(!is.na(species$infected_countries))) {
-    countries <- unique(unlist(strsplit(setdiff(species$infected_countries, NA), 
+    countries <- unique(unlist(strsplit(setdiff(species$infected_countries, NA),
                                  '\\s*,\\s*')))
     testmatch <- countrycode::countrycode(
       countries, 'country.name', 'iso3n', warn=FALSE)
-    
+
     if(any(is.na(testmatch))) {
       stop('Unrecognised countries:\n    - ',
            paste0(countries[is.na(testmatch)],
@@ -152,15 +152,15 @@ excel_to_plan <- function(file) {
            '\nSee countrycode::codelist$country.name.en for allowed values.')
     }
   }
-  
+
   n <- nrow(species)
   plans <- lapply(seq_len(n), function(i) {
     x <- as.list(species[i, ])
     x <- x[!sapply(x, is.na)]
     x[sapply(x, is.list)] <- lapply(x[sapply(x, is.list)], unlist)
-    
+
     # split probabilities and coerce to numeric vector
-    x[grep('^prob_', names(x))] <- 
+    x[grep('^prob_', names(x))] <-
       lapply(x[grep('^prob_', names(x))], function(y) {
         as.numeric(strsplit(as.character(y), ',\\s*')[[1]])
       })
@@ -170,7 +170,7 @@ excel_to_plan <- function(file) {
       stop('When multiple probabilities are passed for a given pathway, ',
            'this number must be consistent across pathways.')
     }
-    
+
     if('gbif_species' %in% names(x)) {
       if(!'use_gbif' %in% names(x) || x$use_gbif) {
         x$gbif_species <- gsub('^\\s+|\\s+$', '', x$gbif_species)
@@ -178,45 +178,45 @@ excel_to_plan <- function(file) {
         x$gbif_species <- strsplit(x$gbif_species, '\\s*,\\s*')[[1]]
       }
     }
-    
+
     if('infected_countries' %in% names(x)) {
       x$infected_countries <- gsub('^\\s+|\\s+$', '', x$infected_countries)
       x$infected_countries <- strsplit(x$infected_countries, '\\s*,\\s*')[[1]]
     }
-    
+
     if(length(x$clum_classes)==0) x$clum_classes <- NULL
     if(length(x$nvis_classes)==0) x$nvis_classes <- NULL
     x$gbif_username <- sub('^\\s+$', '', globals$gbif_username)
     x$gbif_password <- sub('^\\s+$', '', globals$gbif_password)
     args <-  c(
-      x[setdiff(names(x), c('include_nvis', 'gbif_username', 'gbif_password', 
-                            'species_group'))], 
+      x[setdiff(names(x), c('include_nvis', 'gbif_username', 'gbif_password',
+                            'species_group'))],
       globals[intersect(names(globals), names(formals(species_plan)))])
     if(is.na(args$gbif_username)) args$gbif_username <- NULL
     if(is.na(args$gbif_password)) args$gbif_password <- NULL
     if(is.na(args$basemap_mode)) args$basemap_mode <- 'osm'
     do.call(species_plan, args)
   })
-  
+
   # Group species if necessary
-  groups <- species[!is.na(gsub('^\\s+|\\s+$', '', species$species_group)), 
+  groups <- species[!is.na(gsub('^\\s+|\\s+$', '', species$species_group)),
                     c('species', 'species_group')]
   if(nrow(groups > 0)) {
     plans$group_plans <- lapply(unique(groups$species_group), function(x) {
       group <- gsub('\\s+', '_', x)
       species_in_group <- groups$species[groups$species_group==x]
-      ff <- sprintf('outputs/%1$s/%1$s_edmap_%2$s.tif', 
+      ff <- sprintf('outputs/%1$s/%1$s_edmap_%2$s.tif',
                     gsub(' ', '_', species_in_group), 1000)
-      f_out <- sprintf('outputs/%1$s/%1$s_group_edmap_%2$s.tif', 
+      f_out <- sprintf('outputs/%1$s/%1$s_group_edmap_%2$s.tif',
                        gsub(' ', '_', group), 1000)
       res <- c(1000, 1000) # enforce 1km for now - memory safe
       aggregated_res <- c(5000, 5000) # hardcoding for now (for grouped species' maps)
       agg_factor <- aggregated_res[1]/res[1]
       group_plan <- drake::drake_plan(
-        group_establishment_likelihood = 
-          calculate_median(files=drake::file_in(!!ff), 
+        group_establishment_likelihood =
+          calculate_median(files=drake::file_in(!!ff),
                            outfile=drake::file_out(!!f_out)),
-        
+
         plot_group_national_establishment_likelihood = static_map(
           ras = drake::file_in(
             !!sprintf("outputs/%s/%s_group_edmap_%s.tif", group, group, res[1])
@@ -226,17 +226,17 @@ excel_to_plan <- function(file) {
           legend_title = !!sprintf("log10(EL %skm)", round(aggregated_res[1]/1000, 2)),
           set_value_range = c(10^-8, Inf),
           scale_type = "log10",
-          transparency = 1, 
-          aggregate_raster = list(!!agg_factor, max), 
+          transparency = 1,
+          aggregate_raster = list(!!agg_factor, max),
           height = 7,
           layer_names = c("Median", "Min", "Max"),
           nrow = 1,
           outfile = drake::file_out(
-            !!sprintf("outputs/%s/static_maps/%s_group_edmap_national_%s.pdf", 
+            !!sprintf("outputs/%s/static_maps/%s_group_edmap_national_%s.pdf",
                     group, group, aggregated_res[1])
           )
         ),
-        
+
         group_cairns_edmap = static_map(
           ras = drake::file_in(!!sprintf(
             "outputs/%s/%s_group_edmap_%s.tif", group, group, res[1])
@@ -245,7 +245,7 @@ excel_to_plan <- function(file) {
           ylim = c(-17.34, -16.68),
           legend_title = !!sprintf("log10(EL %skm)", round(res[1]/1000, 2)),
           set_value_range = c(10^-8, Inf),
-          scale_type = "log10", 
+          scale_type = "log10",
           transparency = 0.7,
           colramp_entire_range = TRUE,
           height = 7,
@@ -255,7 +255,7 @@ excel_to_plan <- function(file) {
             !!sprintf("outputs/%s/static_maps/%s_group_edmap_cairns_%s.pdf", group, group, res[1])
           )
         ),
-        
+
         group_brisbane_edmap = static_map(
           ras = drake::file_in(!!sprintf(
             "outputs/%s/%s_group_edmap_%s.tif", group, group, res[1])
@@ -264,7 +264,7 @@ excel_to_plan <- function(file) {
           ylim = c(-28.11, -26.45),
           legend_title = !!sprintf("log10(EL %skm)", round(res[1]/1000, 2)),
           set_value_range = c(10^-8, Inf),
-          scale_type = "log10", 
+          scale_type = "log10",
           transparency = 0.7,
           colramp_entire_range = TRUE,
           height = 7,
@@ -274,7 +274,7 @@ excel_to_plan <- function(file) {
             !!sprintf("outputs/%s/static_maps/%s_group_edmap_brisbane_%s.pdf", group, group, res[1])
           )
         ),
-        
+
         group_sydney_edmap = static_map(
           ras = drake::file_in(!!sprintf(
             "outputs/%s/%s_group_edmap_%s.tif", group, group, res[1])
@@ -283,7 +283,7 @@ excel_to_plan <- function(file) {
           ylim = c(-34.25, -33.51),
           legend_title = !!sprintf("log10(EL %skm)", round(res[1]/1000, 2)),
           set_value_range = c(10^-8, Inf),
-          scale_type = "log10", 
+          scale_type = "log10",
           transparency = 0.7,
           colramp_entire_range = TRUE,
           height = 7,
@@ -293,7 +293,7 @@ excel_to_plan <- function(file) {
             !!sprintf("outputs/%s/static_maps/%s_group_edmap_sydney_%s.pdf", group, group, res[1])
           )
         ),
-        
+
         group_melbourne_edmap = static_map(
           ras = drake::file_in(!!sprintf(
             "outputs/%s/%s_group_edmap_%s.tif", group, group, res[1])
@@ -302,7 +302,7 @@ excel_to_plan <- function(file) {
           ylim = c(-38.1, -37.5),
           legend_title = !!sprintf("log10(EL %skm)", round(res[1]/1000, 2)),
           set_value_range = c(10^-8, Inf),
-          scale_type = "log10", 
+          scale_type = "log10",
           transparency = 0.7,
           colramp_entire_range = TRUE,
           height = 7,
@@ -312,7 +312,7 @@ excel_to_plan <- function(file) {
             !!sprintf("outputs/%s/static_maps/%s_group_edmap_melbourne_%s.pdf", group, group, res[1])
           )
         ),
-        
+
         group_hobart_edmap = static_map(
           ras = drake::file_in(!!sprintf(
             "outputs/%s/%s_group_edmap_%s.tif", group, group, res[1])
@@ -321,7 +321,7 @@ excel_to_plan <- function(file) {
           ylim = c(-43.34, -42.33),
           legend_title = !!sprintf("log10(EL %skm)", round(res[1]/1000, 2)),
           set_value_range = c(10^-8, Inf),
-          scale_type = "log10", 
+          scale_type = "log10",
           transparency = 0.7,
           colramp_entire_range = TRUE,
           height = 7,
@@ -331,7 +331,7 @@ excel_to_plan <- function(file) {
             !!sprintf("outputs/%s/static_maps/%s_group_edmap_hobart_%s.pdf", group, group, res[1])
           )
         ),
-        
+
         group_adelaide_edmap = static_map(
           ras = drake::file_in(!!sprintf(
             "outputs/%s/%s_group_edmap_%s.tif", group, group, res[1])
@@ -340,7 +340,7 @@ excel_to_plan <- function(file) {
           ylim = c(-36, -34),
           legend_title = !!sprintf("log10(EL %skm)", round(res[1]/1000, 2)),
           set_value_range = c(10^-8, Inf),
-          scale_type = "log10", 
+          scale_type = "log10",
           transparency = 0.7,
           colramp_entire_range = TRUE,
           height = 7,
@@ -350,7 +350,7 @@ excel_to_plan <- function(file) {
             !!sprintf("outputs/%s/static_maps/%s_group_edmap_adelaide_%s.pdf", group, group, res[1])
           )
         ),
-        
+
         group_perth_edmap = static_map(
           ras = drake::file_in(!!sprintf(
             "outputs/%s/%s_group_edmap_%s.tif", group, group, res[1])
@@ -359,7 +359,7 @@ excel_to_plan <- function(file) {
           ylim = c(-32.56, -31.41),
           legend_title = !!sprintf("log10(EL %skm)", round(res[1]/1000, 2)),
           set_value_range = c(10^-8, Inf),
-          scale_type = "log10", 
+          scale_type = "log10",
           transparency = 0.7,
           colramp_entire_range = TRUE,
           height = 7,
@@ -369,7 +369,7 @@ excel_to_plan <- function(file) {
             !!sprintf("outputs/%s/static_maps/%s_group_edmap_perth_%s.pdf", group, group, res[1])
           )
         ),
-        
+
         group_darwin_edmap = static_map(
           ras = drake::file_in(!!sprintf(
             "outputs/%s/%s_group_edmap_%s.tif", group, group, res[1])
@@ -378,7 +378,7 @@ excel_to_plan <- function(file) {
           ylim = c(-12.88, -12.09),
           legend_title = !!sprintf("log10(EL %skm)", round(res[1]/1000, 2)),
           set_value_range = c(10^-8, Inf),
-          scale_type = "log10", 
+          scale_type = "log10",
           transparency = 0.7,
           colramp_entire_range = TRUE,
           height = 7,
@@ -393,6 +393,6 @@ excel_to_plan <- function(file) {
       group_plan
     }) %>% dplyr::bind_rows()
   }
-  
+
   return(unique(dplyr::bind_rows(plans)))
 }
