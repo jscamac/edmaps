89841a57829cb6352b540da53ad3e07c741cfc04
John Baumgartner <johnbaums@gmail.com>
2021-04-09 00:08:43 +1000

    Update interactive_map as per 96291bc

--- R/interactive_map.R
+++ R/interactive_map.R
@@ -1,42 +1,42 @@
 #' Produce an interactive html map
 #'
 #' Produce an interactive html map.
-#' 
+#'
 #' @param ras A \code{RasterLayer} or file path to a raster file.
-#' @param layer_name Character. An optional name to assign to \code{ras}. 
+#' @param layer_name Character. An optional name to assign to \code{ras}.
 #' @param palette Either a vector of 2 or more colours (e.g. as hex codes or
 #'   colour names) or the name of a palette function supported by \code{tmap}
 #'   (see \code{\link[tmaptools]{palette_explorer}} and
 #'   \code{\link[tmap]{tm_raster}}).
-#' @param transparency Numeric. Value between 0 and 1 defining the opacity of 
+#' @param transparency Numeric. Value between 0 and 1 defining the opacity of
 #'   the plotted raster data (1 = fully opaque; 0 = fully transparent).
 #' @param legend Logical. Should a legend be plotted?
 #' @param set_value_range A numeric vector giving upper and lower limits for
-#'   raster values. Values outside this range (including the limits) will be 
+#'   raster values. Values outside this range (including the limits) will be
 #'   set to NA.
-#' @param discrete Logical. Are the values of \code{ras} discrete 
+#' @param discrete Logical. Are the values of \code{ras} discrete
 #'   (categorical)?
 #' @param scale_type Character. Can be one of: \code{"none"} (raw data, no
-#'   rescaling), \code{"log10"}, \code{"max normalize"} (proportional to 
-#'   maximum value), \code{"minmax normalize"} (rescale values to be between 0 
-#'   and 1 based on min and max) or \code{"logit"}. Note that if 
+#'   rescaling), \code{"log10"}, \code{"max normalize"} (proportional to
+#'   maximum value), \code{"minmax normalize"} (rescale values to be between 0
+#'   and 1 based on min and max) or \code{"logit"}. Note that if
 #'   \code{"log10"} or \code{"logit"} is used, 0 or 1 values must be masked (
-#'   using \code{set_value_range}) or rescaled before passing to this 
+#'   using \code{set_value_range}) or rescaled before passing to this
 #'   function. \code{scale_type} is ignored if \code{discrete} is \code{TRUE}.
 #' @param outfile Character. If \code{NULL}, map will be returned to R and not
 #'   saved. Otherwise, map will be exported as a html file. Full path address
 #'   must be used. If pandoc is available, a standalone html file is created
 #'   (see details).
-#' @param surveillance_locs A spatial object or a path to a .csv file 
+#' @param surveillance_locs A spatial object or a path to a .csv file
 #'   containing columns named "Latitude" and "Longitude".
 #' @param pt_col Character. The plotting colour for surveillance points.
-#' @param cleanup Logical. If a standalone html file is created, should 
-#'   accessory files be removed after the standalone file is generated? This 
+#' @param cleanup Logical. If a standalone html file is created, should
+#'   accessory files be removed after the standalone file is generated? This
 #'   will be a folder created within \code{tempdir()}.
 #' @return A html map.
-#' @details To create a standalone html file, the 
-#'   \href{https://pandoc.org/installing.html}{pandoc} software must be 
-#'   installed and available to R. If pandoc is unavailable, the html file 
+#' @details To create a standalone html file, the
+#'   \href{https://pandoc.org/installing.html}{pandoc} software must be
+#'   installed and available to R. If pandoc is unavailable, the html file
 #'   will be accompanied by a folder of accessory files.
 #' @importFrom raster stack unstack setMinMax minValue maxValue getValues setValues ncell extent projection writeRaster projectRaster
 #' @importFrom gdalUtilities gdalwarp
@@ -52,16 +52,16 @@
 #' @importFrom leaflet addMiniMap
 #' @importFrom stars read_stars
 #' @export
-interactive_map <- function(ras, layer_name = NULL, palette = 'inferno', 
+interactive_map <- function(ras, layer_name = NULL, palette = 'inferno',
   transparency = 0.8, legend = TRUE, set_value_range = NULL,  discrete = FALSE,
-  scale_type = "none", outfile = NULL, surveillance_locs = NULL,  
+  scale_type = "none", outfile = NULL, surveillance_locs = NULL,
   pt_col = "red", cleanup = FALSE) {
-  
+
   # Ensure valid scaling type
   scale_type <- match.arg(
-    scale_type, 
+    scale_type,
     c("none", "log10", "max normalize", "minmax normalize", "logit", "percent"))
-  
+
   if(!is.null(outfile)) {
     if(!grepl('\\.html$', outfile))
       stop('outfile should have a .html extension.')
@@ -70,77 +70,80 @@ interactive_map <- function(ras, layer_name = NULL, palette = 'inferno',
       dir.create(dirname(outfile), recursive = TRUE)
     }
   }
-  
+
   # Must be a path to a raster file, or a RasterLayer
   if(is.character(ras)) ras <- raster::stack(ras)
   ras <- raster::setMinMax(ras)
-  
-  
+
+
   # If projection is missing, assume WGS84
   if(is.na(raster::projection(ras))) {
     warning('ras assumed to have WGS84 coordinate system (EPSG:4326)')
     raster::projection(ras) <- '+init=epsg:4326'
   }
-  
+
   # if palette has length = 1, it must be a supported palette name.
   if(length(palette) == 1) palette <-
     match.arg(palette, rownames(tmaptools::tmap.pal.info))
-  
-  
+
+
   # Restrict Raster value range?
   if(!is.null(set_value_range)) {
     ras <- raster::stack(lapply(raster::unstack(ras), function(x) {
-      x[x <= min(set_value_range) | x >= max(set_value_range)] <- NA  
+      x[x <= min(set_value_range) | x >= max(set_value_range)] <- NA
       x
     }))
   }
-  
-  if(!isTRUE(discrete)) {
-    # Convert to log10 scale
-    if(scale_type == "log10") {
-      if(any(raster::getValues(ras) <= 0, na.rm=TRUE)) {
-        stop('Cannot log transform raster containing zero or negative values.')
-      } else { 
-        ras <- raster::setValues(ras, log10(raster::getValues(ras)))
+
+  # if the raster contains non-zero values...
+  if(!is.na(raster::minValue(ras))) {
+    if(!isTRUE(discrete)) {
+      # Convert to log10 scale
+      if(scale_type == "log10") {
+        if(any(raster::getValues(ras) <= 0, na.rm=TRUE)) {
+          stop('Cannot log transform raster containing zero or negative values.')
+        } else {
+          ras <- raster::setValues(ras, log10(raster::getValues(ras)))
+        }
       }
-    }
-    
-    # Max normalize
-    if(scale_type == "max normalize") {
-      ras <- max_normalize(ras)
-    }
-    # Min Max normalize
-    if(scale_type == "minmax normalize") {
-      ras <- min_max_normalize(ras)
-    }
-    
-    # Convert to logit scale
-    if(scale_type == "logit") {
-      if(any(
-        raster::getValues(ras) <= 0 | raster::getValues(ras) >= 1, na.rm=TRUE)
-      ) {
-        stop('Cannot logit transform raster containing values less than or',
-             ' equal to 0 or values greater than or equal to 1.')
-      } else {
-        ras <- raster::setValues(
-          ras, stats::qlogis(raster::getValues(ras))
-        )
+
+      # Max normalize
+      if(scale_type == "max normalize") {
+        ras <- max_normalize(ras)
       }
-    }
-    
-    # Convert to percent scale
-    if(scale_type == "percent") {
-      if(any(
-        raster::getValues(ras) <0 | raster::getValues(ras) >1, na.rm=TRUE)
-      ) {
-        stop('Cannot convert to percentage raster because there are values',
-             ' less than 0 or values greater 1.')
-      } else {
-        ras <-raster::setValues(ras, raster::getValues(ras)*100)
+      # Min Max normalize
+      if(scale_type == "minmax normalize") {
+        ras <- min_max_normalize(ras)
+      }
+
+      # Convert to logit scale
+      if(scale_type == "logit") {
+        if(any(
+          raster::getValues(ras) <= 0 | raster::getValues(ras) >= 1, na.rm=TRUE)
+        ) {
+          stop('Cannot logit transform raster containing values less than or',
+               ' equal to 0 or values greater than or equal to 1.')
+        } else {
+          ras <- raster::setValues(
+            ras, stats::qlogis(raster::getValues(ras))
+          )
+        }
+      }
+
+      # Convert to percent scale
+      if(scale_type == "percent") {
+        if(any(
+          raster::getValues(ras) <0 | raster::getValues(ras) >1, na.rm=TRUE)
+        ) {
+          stop('Cannot convert to percentage raster because there are values',
+               ' less than 0 or values greater 1.')
+        } else {
+          ras <-raster::setValues(ras, raster::getValues(ras)*100)
+        }
       }
     }
   }
-  
+
   # Flip legend
   flip_legend <- function(m) {
     # m: a `mapview` or `leaflet` object
@@ -149,7 +152,7 @@ interactive_map <- function(ras, layer_name = NULL, palette = 'inferno',
     } else if((methods::is(m, 'leaflet'))) {
       calls <- m$x$calls
     } else {
-      stop('m must be a mapview or leaflet object.')    
+      stop('m must be a mapview or leaflet object.')
     }
     i <- grep('addLegend', sapply(calls, '[[', 'method'))
     clr <- calls[[i]]$args[[1]]$colors
@@ -163,23 +166,23 @@ interactive_map <- function(ras, layer_name = NULL, palette = 'inferno',
       clr2 <- paste0(do.call(paste, list(rev(h), c('', p, ''))), collapse=', ')
       calls[[i]]$args[[1]]$colors <- clr2
       calls[[i]]$args[[1]]$labels <- rev(calls[[i]]$args[[1]]$labels)
-      extra <- calls[[i]]$args[[1]]$extra 
+      extra <- calls[[i]]$args[[1]]$extra
       calls[[i]]$args[[1]]$extra$p_1 <- 1 - extra$p_n
       calls[[i]]$args[[1]]$extra$p_n <- 1 - extra$p_1
       if(methods::is(m, 'mapview')) {
         methods::slot(m, 'map')$x$calls <- calls
       } else if((methods::is(m, 'leaflet'))) {
         m$x$calls <- calls
-      } 
+      }
     }
     m
   }
-  
+
   # Initialise interactive map
   opts <- tmap::tmap_options(
-    max.raster=c(plot=raster::ncell(ras), 
+    max.raster=c(plot=raster::ncell(ras),
                  view=raster::ncell(ras)),
-    basemaps = c(OpenStreetMap="OpenStreetMap", Canvas = "Esri.WorldGrayCanvas", 
+    basemaps = c(OpenStreetMap="OpenStreetMap", Canvas = "Esri.WorldGrayCanvas",
                  Topo="Esri.WorldTopoMap", Imagery = "Esri.WorldImagery"),
     overlays = c(Labels = paste0(
       "http://services.arcgisonline.com/arcgis/rest/services/Canvas/",
@@ -192,24 +195,24 @@ interactive_map <- function(ras, layer_name = NULL, palette = 'inferno',
   })
   if(isTRUE(discrete)) {
     ras <- raster::projectRaster(ras, crs = "+init=epsg:3857", method = "ngb")
-    m <- tmap::tm_shape(ras, name=layer_name) + 
-      tmap::tm_raster(palette=palette, style='cat', title=layer_name, 
+    m <- tmap::tm_shape(ras, name=layer_name) +
+      tmap::tm_raster(palette=palette, style='cat', title=layer_name,
                       alpha=transparency)
   } else {
     raster::writeRaster(ras, f <- tempfile(fileext='.tif'))
-    gdalUtilities::gdalwarp(f, f2 <- tempfile(fileext='.tif'), 
+    gdalUtilities::gdalwarp(f, f2 <- tempfile(fileext='.tif'),
                             t_srs = "+init=epsg:3857", r = "bilinear")
     ras <- stars::read_stars(f2)
     nbands <- ifelse(length(dim(ras))==2, 1, dim(ras)[3])
     m <- tmap::tm_shape(ras, name = layer_name) +
       tmap::tm_raster(palette = palette,
-                      style = "cont", midpoint = NA, alpha = transparency, 
-                      legend.show=c(TRUE, rep(FALSE, nbands - 1)), 
-                      title = layer_name) + 
+                      style = "cont", midpoint = NA, alpha = transparency,
+                      legend.show=c(TRUE, rep(FALSE, nbands - 1)),
+                      title = layer_name) +
       tmap::tm_facets(as.layers=TRUE)
   }
-  
-  
+
+
   # Add trap locations (if required)
   if(!is.null(surveillance_locs)) {
     if(is.character(surveillance_locs)) {
@@ -218,27 +221,27 @@ interactive_map <- function(ras, layer_name = NULL, palette = 'inferno',
                       !is.na(Latitude))
     }
     locs <- suppressWarnings(
-      sf::st_as_sf(surveillance_locs, coords = c("Longitude","Latitude"), 
+      sf::st_as_sf(surveillance_locs, coords = c("Longitude","Latitude"),
                    crs = 4326) %>%
         sf::st_transform(crs = sf::st_crs(ras)) %>%
         sf::st_crop(y = raster::extent(ras))
     )
-    
+
     pts <- tmap::tm_shape(locs) +
       tmap::tm_dots(col=pt_col, shape=21)
-    
+
     m <- m + pts
   }
-  
+
   # Convert tmap to leaflet, flip legend and add functionality
-  l <- (m + tmap::tm_scale_bar()) %>% 
+  l <- (m + tmap::tm_scale_bar()) %>%
     tmap::tmap_leaflet()
-  
+
   out <- l %>%
-    flip_legend %>% 
-    leaflet::addMiniMap(position='bottomleft', toggleDisplay=TRUE) %>% 
+    flip_legend %>%
+    leaflet::addMiniMap(position='bottomleft', toggleDisplay=TRUE) %>%
     leafem::addMouseCoordinates()
-  
+
   # outfile supplied
   if(!is.null(outfile)) {
     # If pandoc is installed, make self-contained html.
@@ -256,8 +259,8 @@ interactive_map <- function(ras, layer_name = NULL, palette = 'inferno',
       }
     } else {
       htmlwidgets::saveWidget(out, selfcontained=FALSE,
-                    file=file.path(normalizePath(dirname(outfile)), 
-                                   basename(outfile)))  
+                    file=file.path(normalizePath(dirname(outfile)),
+                                   basename(outfile)))
     }
   } else {
     # Else return object in R
