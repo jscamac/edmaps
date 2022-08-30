f4ab5431156073edd51ee1961cd4d9426481fbaf
John Baumgartner <johnbaums@gmail.com>
2022-08-30 20:49:18 +1000

    Update url and variables

--- R/download_worldclim2.R
+++ R/download_worldclim2.R
@@ -1,15 +1,18 @@
-#' Download climate layers from WorldClim 2.0
+#' Download climate layers from WorldClim 2.1
 #'
-#' Download climate layers from WorldClim 2.0 (current climate only).
+#' Download historical gridded climate data from WorldClim 2.1
 
 #' @param outfile Character. Target file for downloaded .zip archive.
 #' @param variable Character. Can be one of `"bio"` (19 standard bioclim
-#'   variables), `"tmin"`, `"tmax"`, `"tavg"`, `"srad"`,
-#'   `"wind"` or `"vapr"`.
-#' @param resolution Character. Can be one of `"10m"`, `"5m"`,
-#'   `"2.5m"`, or `"30s"`.
+#'   variables), `"tmin"`, `"tmax"`, `"tavg"`, `"prec"`, `"srad"`, `"wind"`,
+#'   `"vapr"`, or `"elev"`.
+#' @param resolution Character. Can be one of `"10m"`, `"5m"`, `"2.5m"`, or
+#'   `"30s"`.
 #' @return A zipfile is downloaded and `NULL` is returned invisibly.
 #' @seealso [extract_worldclim2()]
+#' @references Fick, S.E. and R.J. Hijmans, 2017. WorldClim 2: new 1km spatial
+#'   resolution climate surfaces for global land areas. International Journal of
+#'   Climatology 37 (12): 4302-4315.
 #' @importFrom utils download.file
 #' @export
 #' @examples
@@ -19,7 +22,9 @@
 download_worldclim2 <- function(outfile, variable, resolution) {
 
   variable <- match.arg(
-    variable, c("bio", "tmin", "tmax", "tavg", "srad", "wind", "vapr"))
+    variable,
+    c("bio", "tmin", "tmax", "tavg", "prec", "srad", "wind", "vapr", "elev")
+  )
   resolution <- match.arg(resolution, c("10m", "5m","2.5m","30s"))
 
   if(!dir.exists(dirname(outfile))) dir.create(dirname(outfile))
