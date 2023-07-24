b81cac868557fb6ff6f1de193475ba5edf1326bc
John Baumgartner <johnbaums@gmail.com>
2023-07-25 00:10:08 +1000

    Replace future with crew

--- _targets.R
+++ _targets.R
@@ -1,14 +1,13 @@
 library(targets)
 library(dplyr)
-library(future)
-library(future.apply)
+library(crew)
 library(parallelly)
 library(edmaps)
 
 n_cores <- max(1, floor(parallelly::availableCores(constraints='multicore') * 0.75))
-tar_option_set(packages = c('terra', 'edmaps', 'future', 'future.apply', 'dplyr'))
+tar_option_set(packages = c('terra', 'edmaps', 'future', 'future.apply', 'dplyr'),
+               controller=crew::crew_controller_local(workers=n_cores))
 message('no. of cores: ', n_cores)
-future::multicore(workers = n_cores)
 source('R/get_gbif_records.R')
 source('functions.R')
 make_plan('inst/extdata/parameters.xlsx')
